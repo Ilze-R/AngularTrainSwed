980af4334b15f2512db809411445facb07d236b9
Ilze Rigerte <irigerte@gmail.com>
2023-03-22 17:47:12 +0200

    first task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PersonInfoComponent } from './person-info/person-info.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HomeComponent, PersonInfoComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/person-info/person-info.component.ts
@@ -0,0 +1,36 @@
+import {
+  AfterContentInit,
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+import { Person } from '../types/person';
+
+@Component({
+  selector: 'app-person-info',
+  templateUrl: './person-info.component.html',
+  styleUrls: ['./person-info.component.css'],
+})
+export class PersonInfoComponent
+  implements OnInit, AfterContentInit, OnChanges
+{
+  @Input() personAttribute: Person;
+
+  constructor() {
+    console.log('constructor');
+    console.log('person attribute: ' + this.personAttribute);
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges');
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+}
